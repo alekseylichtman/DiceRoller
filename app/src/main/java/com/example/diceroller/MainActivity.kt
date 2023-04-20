cd4c01dca879cbdbeaa43f602be0080cbf3aea72
Aleksey Lichtman <alichtman@sram.com>
2023-04-20 13:02:42 -0500

    completed dice roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,28 +2,49 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    val rollButton: Button = findViewById(R.id.rollButton)
-    val resultTextView: TextView = findViewById(R.id.resultTextView)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val rollButton: Button = findViewById(R.id.rollButton)
         rollButton.setOnClickListener {
+            roll()
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
-            resultTextView.text = roll().toString()
         }
     }
-    
-    fun roll(sides: Int = 6): Int {
-        return (1..sides).random()
+
+    fun roll(sides: Int = 6) {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val diceImageView: ImageView = findViewById(R.id.diceImageView)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> { return }
+        }
+
+        diceImageView.setImageResource(drawableResource)
+        diceImageView.contentDescription = diceRoll.toString()
     }
 
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
 }
