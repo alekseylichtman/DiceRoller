cab11527f3326e6d4d35ea10091e98411f9152b4
Aleksey Lichtman <alichtman@sram.com>
2023-04-20 13:17:00 -0500

    added unit test

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.diceroller
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,12 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    fun generates_bumber() {
+        val maxValue = 6
+        val dice = Dice(6)
+        val result = dice.roll()
+        assertTrue("The value i snot in range between 1 and max value (${maxValue}", result <= maxValue)
     }
 }
